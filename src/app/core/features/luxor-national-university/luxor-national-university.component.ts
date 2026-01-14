91e976c4c010d18610d89f9056a8e2d6043e135a
sama152000 <sama.sama152000@gmail.com>
2026-01-14 07:32:58 +0200

    first commit

---
+++ src/app/core/features/luxor-national-university/luxor-national-university.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-luxor-national-university',
+  templateUrl: './luxor-national-university.component.html',
+  styleUrls: ['./luxor-national-university.component.css']
+})
+export class LuxorNationalUniversityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://archaeologyar.runasp.net/api/v1/'  
+};
