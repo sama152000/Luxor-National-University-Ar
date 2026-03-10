f2d4a90a576b1f33b1434e56b88b51b9a0b071bf
sama152000 <sama.sama152000@gmail.com>
2026-03-10 07:04:57 +0200

    lang

--- src/app/core/features/luxor-national-university/Services/common.service.ts
+++ src/app/core/features/luxor-national-university/Services/common.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ContactInfo, Language } from '../model/common.model';
+import { ContactService } from './contact.service';
+import { Contact } from '../model/contact.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +10,34 @@ import { ContactInfo, Language } from '../model/common.model';
 export class CommonService {
   private currentLanguageSubject = new BehaviorSubject<string>('ar');
   public currentLanguage$: Observable<string> = this.currentLanguageSubject.asObservable();
+  
+  private contactInfoSubject = new BehaviorSubject<ContactInfo | null>(null);
+  public contactInfo$: Observable<ContactInfo | null> = this.contactInfoSubject.asObservable();
 
-  constructor() {}
+  constructor(private contactService: ContactService) {
+    this.loadContactInfo();
+  }
 
-  getContactInfo(): ContactInfo {
-    return {
-      phone: '+20 88 123 4567',
-      email: 'info@lnu.edu.eg',
-      website: 'www.lnu.edu.eg'
-    };
+  private loadContactInfo(): void {
+    this.contactService.getAllContacts().subscribe({
+      next: (contacts: Contact[]) => {
+        if (contacts && contacts.length > 0) {
+          const contact = contacts[0];
+          this.contactInfoSubject.next({
+            phone: contact.phone,
+            email: contact.email,
+            website: contact.webSite
+          });
+        }
+      },
+      error: () => {
+        // Keep null on error - data will come from API
+      }
+    });
+  }
+
+  getContactInfo(): ContactInfo | null {
+    return this.contactInfoSubject.value;
   }
 
   getLanguages(): Language[] {
@@ -28,7 +49,6 @@ export class CommonService {
 
   switchLanguage(languageCode: string): void {
     this.currentLanguageSubject.next(languageCode);
-    // Implement language switching logic here
     document.dir = languageCode === 'ar' ? 'rtl' : 'ltr';
   }
 
